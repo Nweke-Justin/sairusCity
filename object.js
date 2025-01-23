9d53204c8034ae47b759df01abe3d8aef0ceed0e
Nweke-Justin <nwekejustin580@gmail.com>
2025-01-23 08:08:19 -0800

    i added to files

---
+++ object.js
@@ -0,0 +1,11 @@
+const student={
+    name:"Nweke Justin",
+    age:21,
+    subjects:["biology","chemistry","physics","maths"],
+    address:{
+        street:"babalola",
+        city:"lagos",
+        state:"lagos state"
+  }
+};
+console.log(student.address.city);
